2a00eeede9ac0b26c82e6bcd1ae1cc53599e406a
rafguevara <rafguevara14@gmail.com>
2022-11-26 16:02:04 -0800

    add gio set function

--- fw-lib/fw_gpio.c
+++ fw-lib/fw_gpio.c
@@ -4,3 +4,15 @@ void fw_gpio_init()
 {
 	gioInit();
 }
+
+void fw_gpio_set(gioPORT_t *port, uint32 bit, eGPIOStates state)
+{
+	if (state == FW_GPIO_TOGGLE)
+	{
+		gioToggleBit(port, bit);
+	}
+	else
+	{
+		gioSetBit(port, bit, state);
+	}
+}
--- fw-lib/fw_gpio.h
+++ fw-lib/fw_gpio.h
@@ -3,6 +3,15 @@
 
 #include "hardware_peripherals.h"
 
+typedef enum eGPIOStates
+{
+	FW_GPIO_OFF = 0,
+	FW_GPIO_ON = 1,
+	FW_GPIO_TOGGLE = 2
+
+} eGPIOStates;
+
 void fw_gpio_init();
+void fw_gpio_set(gioPORT_t *port, uint32 bit, eGPIOStates state);
 
 #endif
