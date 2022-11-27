1e4b71ae61f7faebfcefda2cecb3c2740a6cd12a
rafguevara <rafguevara14@gmail.com>
2022-11-26 16:01:47 -0800

    cleanup example

--- main.c
+++ main.c
@@ -13,33 +13,35 @@
 volatile uint32_t tick = 0;
 void global_clock()
 {
-	if (tick % MS_TO_HW_TICKS(100, HW_TIMER0_PERIOD_MS) == 0)
+	if (tick % MS_TO_HW_TICKS(1000, HW_TIMER0_PERIOD_MS) == 0)
 	{
-		gioToggleBit(USER_LEDA);
+		fw_gpio_set(USER_LEDA, FW_GPIO_TOGGLE);
+		fw_gpio_set(USER_LEDB, FW_GPIO_TOGGLE);
 	}
+	
+
 	tick++;
 }
 
 
 int main(void)
 {
+	/* Initialize hardware according to hardware_peripherals.h */
+	fw_timer_init(HW_TIMER_COMPARE0, global_clock);
 	fw_gpio_init();
 	PCInit(9600);
 
-	gioSetBit(USER_LEDA, 0);
-	gioSetBit(USER_LEDB, 1);
+	/* Set leds to opposite values */
+	fw_gpio_set(USER_LEDA, FW_GPIO_OFF);
+	fw_gpio_set(USER_LEDB, FW_GPIO_ON);
 
-	fw_timer_init(HW_TIMER_COMPARE0, global_clock, 1);
-	
-	_enable_IRQ();
-
+	/* start hardware timer */
 	fw_timer_counter(HW_TIMER_COUNTER0, true);
+
 	while(1)
 	{
         fwprintf("Hello World! (%d)", tick);
 	}
-
-
 
     return 0;
 }
