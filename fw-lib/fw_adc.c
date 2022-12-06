b2e104f04806d86b4b68d6d0647e55fc502f3f57
rafguevara <rafguevara14@gmail.com>
2022-12-05 23:32:26 -0800

    add adc light sensor support

---
+++ fw-lib/fw_adc.c
@@ -0,0 +1,24 @@
+/*
+ * fw_adc.c
+ *
+ *  Created on: Dec 5, 2022
+ *      Author: rafgu
+ */
+
+#include "fw_adc.h"
+
+void fw_adc_init()
+{
+	adcInit();
+}
+
+uint16_t fw_poll_light_sensor()
+{
+	adcStartConversion(LIGHT_SENSOR);
+	while(!adcIsConversionComplete(LIGHT_SENSOR));
+
+	adcData_t data;
+	adcGetData(LIGHT_REG, 1U, &data);
+
+	return ADC_TO_VOLTAGE(data.value, LIGHT_MAX_VAL, LIGHT_REFERENCE_MV);
+}
---
+++ fw-lib/fw_adc.h
@@ -0,0 +1,17 @@
+/*
+ * fw_adc.h
+ *
+ *  Created on: Dec 5, 2022
+ *      Author: rafgu
+ */
+
+#ifndef FW_LIB_FW_ADC_H_
+#define FW_LIB_FW_ADC_H_
+
+#include "hardware_peripherals.h"
+
+void fw_adc_init();
+uint16_t fw_poll_light_sensor();
+
+
+#endif /* FW_LIB_FW_ADC_H_ */
